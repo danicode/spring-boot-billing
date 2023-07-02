aacd7646734d9a32034b1bace5c5da05265ff506
danicode <danicode@danicode-desktop>
2023-07-02 11:41:41 -0300

    Primer commit. ABM Cliente, ABM Factura. Utilizando como capa de persistencia Hibernate y para la seguridad Spring Security.

---
+++ src/main/java/com/bolsadeideas/springboot/app/auth/handler/LoginSuccessHandler.java
@@ -0,0 +1,36 @@
+package com.bolsadeideas.springboot.app.auth.handler;
+
+import java.io.IOException;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.FlashMap;
+import org.springframework.web.servlet.support.SessionFlashMapManager;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws IOException, ServletException {
+		// Obtener el session flash map manager
+		SessionFlashMapManager flashMapManager = new SessionFlashMapManager();
+
+		FlashMap flashMap = new FlashMap();
+		flashMap.put("success", "Hola ".concat(authentication.getName().concat(", haz iniciado sesión con éxito")));
+
+		flashMapManager.saveOutputFlashMap(flashMap, request, response);
+
+		if (authentication != null) {
+			logger.info("El usuario '".concat(authentication.getName().concat("', ha iniciado sesión con éxito")));
+		}
+
+		super.onAuthenticationSuccess(request, response, authentication);
+	}
+
+}
