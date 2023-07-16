045c66f199bc7c825542e6072ab47f30b3b8cb03
danicode <danielanzawa@gmail.com>
2023-07-16 12:08:48 -0300

    seccion-24-wildlfy: Modificación de path para buscar productos por AJAX para que funcione con empaquetado war.

--- src/main/java/com/bolsadeideas/springboot/app/SpringSecurityConfig.java
+++ src/main/java/com/bolsadeideas/springboot/app/SpringSecurityConfig.java
@@ -4,24 +4,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import com.bolsadeideas.springboot.app.auth.filter.JWTAuthenticationFilter;
-import com.bolsadeideas.springboot.app.auth.filter.JWTAuthorizationFilter;
-import com.bolsadeideas.springboot.app.auth.service.JWTService;
+//import com.bolsadeideas.springboot.app.auth.filter.JWTAuthenticationFilter;
+//import com.bolsadeideas.springboot.app.auth.filter.JWTAuthorizationFilter;
+import com.bolsadeideas.springboot.app.auth.handler.LoginSuccessHandler;
+//import com.bolsadeideas.springboot.app.auth.service.JWTService;
 import com.bolsadeideas.springboot.app.models.service.JpaUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SpringSecurityConfig {
-
+	
+	@Autowired
+	private LoginSuccessHandler successHandler;
 	
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
@@ -29,11 +32,11 @@ public class SpringSecurityConfig {
 	 @Autowired
      private JpaUserDetailsService userDetailService;
 	 
-	 @Autowired
-     private AuthenticationConfiguration authenticationConfiguration;
+	 //@Autowired
+     //private AuthenticationConfiguration authenticationConfiguration;
 	 
-	 @Autowired
-	 private JWTService jwtService;
+	 //@Autowired
+	 //private JWTService jwtService;
 	 
 	 @Autowired
 	 public void userDetailsService(AuthenticationManagerBuilder build) throws Exception {
@@ -44,7 +47,7 @@ public class SpringSecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-		http.csrf(
+		/**http.csrf(
 			csrf -> csrf.disable()
 		).authorizeHttpRequests(authz -> authz
     		.requestMatchers("/", "/css/**", "/js/**", "/images/**", "/listar**", "/listarx", "/locale", "/api/clientes/listar**").permitAll()
@@ -59,7 +62,25 @@ public class SpringSecurityConfig {
 			session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 		);
 
-		return http.build();
+		return http.build();*/
+		
+		http.authorizeHttpRequests(authz -> authz
+	    		.requestMatchers("/", "/css/**", "/js/**", "/images/**", "/listar", "/locale").permitAll()
+	            .anyRequest().authenticated()
+	        )
+	        .formLogin(formLogin -> formLogin
+	    		.successHandler(successHandler)
+	            .loginPage("/login")
+	            .permitAll()
+	        )
+	        .logout(logout -> logout
+	    		.logoutSuccessUrl("/login?logout")
+	    		.permitAll()
+			).exceptionHandling((exceptionHandling) -> exceptionHandling
+				.accessDeniedPage("/error_403")
+			);
+
+			return http.build();
 	}
 	
 }
